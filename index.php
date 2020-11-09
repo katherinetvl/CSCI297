951c664ae0fdc9a22910cd0282a68f11b61bdf1d
katherinetvl <63134770+katherinetvl@users.noreply.github.com>
2020-11-09 16:48:59 -0500

    In progress index.php

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
     $cookieVisited = $_COOKIE['visited'];
-    //echo "Cookie named " . $cookieVisited . " is set! <br>";
-    // var_dump($cookieVisited); 
-    $decodeCookieString = base64_decode($cookieVisited);
-    $visitedArr[] = explode(",", $decodeCookieString);
-    // var_dump($visitedArr);
+    $cookieVisited = str_replace("Initial", "", $cookieVisited);
+    var_dump($cookieVisited); 
+    echo "<br> Above is cookieVisited <br>";
+    echo "<br>";
+
+    $decodedCookieString = base64_decode($cookieVisited);
+    var_dump($decodedCookieString);
+    echo "<br> Above is decodedCookieString <br>";
+    echo "<br>";
+    
+    $visitedArr[] = explode(",", $decodedCookieString);
+    var_dump($visitedArr);
+    echo "<br> Above is visited array: <br>";
 ?>
 <?php
 $callListResouce = fopen("callList.csv", "r");
@@ -34,32 +42,12 @@ fclose($callListResouce);
 <ul>
 <?php
 
-// var_dump($visitedArr);
-
-/*
-    foreach($visitedArr as $visitedItem)
-    {
-        foreach($companies as $key => $value)
-        {
-            $valueVisited = trim($value[1]);
-            if($valueVisited == $visitedItem[1])
-            {
-                echo "<li><a href='details.php?company=" . urlencode($key) . "'>" . $value[0] . "- visited" . "</a></li>";
-            }
-            else
-            {
-                echo "<li><a href='details.php?company=" . urlencode($key) . "'>" . $value[0] . "</a></li>";
-            }
-        } 
-    }
-*/
-
-foreach($visitedArr as $item)
+foreach($visitedArr as $itemsVisited)
 {
     foreach($companies as $key => $value)
     {
-        $valueVisited = trim($value[1]);
-        if(in_array($valueVisited, $item))
+        echo "key: " . $key; 
+        if(in_array($key, $itemsVisited))
         {
             echo "<li><a href='details.php?company=" . urlencode($key) . "'>" . $value[0] . "- visited" . "</a></li>";
         }
@@ -68,7 +56,6 @@ foreach($visitedArr as $item)
             echo "<li><a href='details.php?company=" . urlencode($key) . "'>" . $value[0] . "</a></li>";
         }
     } 
-
 }
 
 
